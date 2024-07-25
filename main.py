9fbc3869a7a29411fa2a73c0cf2a87f55ea57403
Marcelo Nascimento <marcelojunior@usp.br>
2024-07-25 19:16:28 -0300

    chore: configurado cors

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from db.database import init_db
 from vaga.routes import router as Vagas
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
