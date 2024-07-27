da48b5f398f7b40a919e88399e3823da8928eae8
Marcelo Nascimento <marcelojunior@usp.br>
2024-07-27 02:11:04 -0300

    chore: rota update vaga

--- dto/models.py
+++ dto/models.py
@@ -1,9 +1,16 @@
 from typing import Optional, Any
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ReqNovaVaga(BaseModel):
     id: str
-    tokens: list[str]
+    tokens: list[str] = []
     isApplied: bool = False
     isEnabled: bool = False
+    txtVaga: str
+
+
+class ReqUpdateVaga(BaseModel):
+    isApplied: bool = None
+    isEnabled: bool = None
+    tokens: list[str] = None
--- vaga/routes.py
+++ vaga/routes.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Body
-from dto.models import ReqNovaVaga
+from fastapi import APIRouter, Body, HTTPException
+
+from dto.models import ReqNovaVaga, ReqUpdateVaga
 from vaga.model import Vaga
 
 router = APIRouter()
@@ -9,18 +10,51 @@ router = APIRouter()
 async def findAll():
     lista = await Vaga.find_all().to_list()
     return {
-        "message": "lista de vagas",
+        "message": "Lista de vagas",
         "data": lista
+    }
+
+@router.get("/{id}", status_code=200)
+async def findById(id: str):
+    vaga = await Vaga.find_one({"_id": id})
+    return {
+        "message": "Vaga.",
+        "data": vaga
     }
 
 
 @router.post("/", status_code=201)
 async def create(vagaReq: ReqNovaVaga = Body(...)):
-    nova_vaga = Vaga(id=vagaReq.id, tokens=vagaReq.tokens, isApplied=vagaReq.isApplied, isEnabled=vagaReq.isEnabled)
+    nova_vaga = Vaga(
+        id=vagaReq.id,
+        tokens=vagaReq.tokens,
+        isApplied=vagaReq.isApplied,
+        isEnabled=vagaReq.isEnabled,
+        txtVaga=vagaReq.txtVaga
+    )
     await nova_vaga.create()
     return {
         "message": "Vaga salva com sucesso.",
         "data": nova_vaga
+    }
+
+@router.put("/{id}", status_code=201)
+async def update(id: str, reqUpdateVaga: ReqUpdateVaga = Body(...)):
+
+    vaga = await Vaga.find_one({"_id": id})
+
+    if not vaga:
+        raise HTTPException(
+            status_code=404
+        )
+
+    reqUpdateVaga = {field: value for field, value in reqUpdateVaga if value is not None}
+    update_query = {"$set": reqUpdateVaga}
+
+    await vaga.update(update_query)
+    return {
+        "message": "Vaga atualizada com sucesso.",
+        "data": vaga
     }
 
 
