85924c103cb48faba0e9f8ebeb51d5fad3d1e00d
Marcelo Nascimento <marcelojunior@usp.br>
2024-07-27 02:10:25 -0300

    fix: host do cors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8080"],
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
