657b0a8e8414fdf49c495ff54ccd27bc42452c89
Marcelo Nascimento <marcelojunior@usp.br>
2024-07-27 14:36:19 -0300

    chore: frontend inicial da api

---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+
+createApp(App).use(router).mount('#app')
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container-fluid">
+    <ul id="lista" class="container-fluid" style="max-width: 800px; list-style-type: none">
+    <li class="mb-4" v-for="vaga in vagas" :key="vaga._id">
+      <div class="row container justify-content-center">
+
+        <div class="col-7">
+          <a v-bind:href="'https://www.linkedin.com/jobs/view/' + vaga._id"
+             target="_blank"
+              style="text-decoration: none;"> {{vaga.txtVaga }}</a>
+        </div>
+
+        <div class="col-2">
+          <div class="form-check form-switch">
+            <input v-model="vaga.isApplied" class="form-check-input" type="checkbox" >
+          </div>
+        </div>
+
+        <div class="col-3">
+          <div>
+            <button @click="atualizar(vaga._id)" type="button" class="btn btn-primary"> Atualizar </button>
+          </div>
+      </div>
+      </div>
+      </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data() {
+    return {
+      vagas: []
+    }
+  },
+  created() {
+    axios.get("http://localhost:8000/api/vagas?updated=false")
+    .then(response => {
+      this.vagas = response.data.data;
+    }).catch(err => {
+      console.log("Erro: ")
+      console.log(err);
+    })
+  },
+  methods: {
+    atualizar(id) {
+      let vaga = this.vagas.filter(vaga => vaga._id === id)[0]
+      axios.put("http://localhost:8000/api/vagas/" + vaga._id, {
+        isApplied: vaga.isApplied,
+        isUpdated: true
+      })
+      .then(response => {
+        console.log("Vaga Atualizada")
+        console.log(response)
+        this.vagas = this.vagas.filter(vaga => vaga._id !== id)
+    }).catch(err => {
+      console.log(err);
+    })
+    }
+  }
+}
+</script>
