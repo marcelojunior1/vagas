8207d6ada3ca4733bb75c771af061333a3cfe2f0
Marcelo Nascimento dos Santos Junior <marcelojunior@usp.br>
2024-09-25 09:13:46 -0300

    chore: mudanca para versao mobile

--- webscraper/background.js
+++ webscraper/background.js
@@ -4,17 +4,14 @@ document.onkeydown = function(evt) {
       return;
     }
 
-    const stringsBloqueadas = ["Candidatou-se", "ex-alunos", "ex-aluno", "verification", "Visualizado", "Recrutando", "corresponde", "horas"]
-    let lista = document.getElementsByClassName("scaffold-layout__list-container")[0].children
+    const stringsBloqueadas = ["Candidatou-se", "Novo", "ex-alunos", "ex-aluno", "verification", "Visualizado", "Recrutando", "corresponde", "horas"]
+    let lista = document.getElementsByClassName("entity-item search-item job-item")
 
-    for (let idx in lista) {
-        let id = lista[idx].dataset.occludableJobId
-        let array_texto = lista[idx].innerText.split('\n')
-        array_texto.splice(1,1)
+    for (let i = 0; i < lista.length; i++) {
+        let id  = lista[i].dataset.id;
+        let array_texto = lista[i].children[2].innerText.split('\n')
         array_texto = array_texto.filter(e => stringsBloqueadas.some(s => e.includes(s)) === false)
-
         let texto = array_texto.reduce((accumulator, current) => accumulator + '\n' + current);
-
         fetch("http://127.0.0.1:8000/api/vagas", {
             method: "POST",
             body: JSON.stringify({
@@ -26,5 +23,5 @@ document.onkeydown = function(evt) {
                 "Content-type": "application/json; charset=UTF-8"
             }
         }).then(r => console.log(r));
-      }
+    }
 };
