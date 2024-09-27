f2612dbb04ae184b3018ac35ce2d2a56fb2bc2c6
Marcelo Nascimento dos Santos Junior <marcelojunior@usp.br>
2024-09-27 13:16:30 -0300

    chore: volta da versao desktop e inclusao de palavra chave

--- webscraper/background.js
+++ webscraper/background.js
@@ -4,12 +4,13 @@ document.onkeydown = function(evt) {
       return;
     }
 
-    const stringsBloqueadas = ["Candidatou-se", "Novo", "ex-alunos", "ex-aluno", "verification", "Visualizado", "Recrutando", "corresponde", "horas"]
-    let lista = document.getElementsByClassName("entity-item search-item job-item")
+    const stringsBloqueadas = ["Candidatou-se", "ex-alunos", "geralmente", "ex-aluno", "verification", "Visualizado", "Recrutando", "corresponde", "horas"]
+    let lista = document.getElementsByClassName("scaffold-layout__list-container")[0].children
 
-    for (let i = 0; i < lista.length; i++) {
-        let id  = lista[i].dataset.id;
-        let array_texto = lista[i].children[2].innerText.split('\n')
+    for (let idx in lista) {
+        let id = lista[idx].dataset.occludableJobId
+        let array_texto = lista[idx].innerText.split('\n')
+        array_texto.splice(1,1)
         array_texto = array_texto.filter(e => stringsBloqueadas.some(s => e.includes(s)) === false)
         let texto = array_texto.reduce((accumulator, current) => accumulator + '\n' + current);
         fetch("http://127.0.0.1:8000/api/vagas", {
@@ -23,5 +24,5 @@ document.onkeydown = function(evt) {
                 "Content-type": "application/json; charset=UTF-8"
             }
         }).then(r => console.log(r));
-    }
+      }
 };
