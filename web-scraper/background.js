466823a1775d6f0c74652ca049afd0b85e544001
Marcelo Nascimento dos Santos Junior <marcelojunior@usp.br>
2024-11-25 21:15:01 -0300

    fix: atualiza novo nome da classe de lista de vagas

--- web-scraper/background.js
+++ web-scraper/background.js
@@ -13,7 +13,7 @@ document.onmousedown = function(evt) {
 
 function scrapper() {
     const stringsBloqueadas = ["Candidatou-se", "ex-alunos", "geralmente", "ex-aluno", "verification", "Visualizado", "Recrutando", "corresponde", "horas"]
-    let lista = document.getElementsByClassName("scaffold-layout__list-container")[0].children
+    let lista = document.getElementsByClassName("OGzrPwbywlJQszAAWcFBQOmSlzEAEeUBo")[0].children
 
     for (let idx in lista) {
         let id = lista[idx].dataset.occludableJobId
