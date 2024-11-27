e57dd29e46eb6f2d34c2764a1abaaec5f3661d60
Marcelo Nascimento dos Santos Junior <marcelojunior@usp.br>
2024-11-27 01:04:14 -0300

    feat: paginacao na lista de vagas

--- web/src/app/vagas/page.tsx
+++ web/src/app/vagas/page.tsx
@@ -10,7 +10,7 @@ import {
     List,
     ListItem,
     ListItemButton,
-    ListItemText
+    ListItemText, TablePagination
 } from "@mui/material";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
@@ -21,8 +21,34 @@ import SendIcon from '@mui/icons-material/Send';
 const url_base: string = "http://localhost:8000/api/vagas/";
 
 export default function Vagas() {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(25);
     const [data, setData] = useState<VagasResDto[] | []>([]);
     const [loading, setLoading] = useState(true);
+
+    const handleChangePage = () => {
+        setPage(0);
+
+        data.forEach((obj, index) => {
+            if (index < rowsPerPage) {
+                const id: string = obj.vaga._id;
+                const url: string = url_base + id;
+                axios.put(url, {
+                    isApplied: false,
+                    isUpdated: true
+                }).then(() => console.log("OK"))
+            }
+        });
+
+        setData(data.filter((obj, index) => index >= rowsPerPage))
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     useEffect(() => {
         const url = "http://127.0.0.1:8000/api/vagas?infer=True&updated=False";
@@ -36,17 +62,6 @@ export default function Vagas() {
                 } )
         }
     })
-
-    function atualizaTudo() {
-        data.forEach((obj) => {
-            const id: string = obj.vaga._id;
-            const url: string = url_base + id;
-            axios.put(url, {
-                isApplied: obj.vaga.isApplied,
-                isUpdated: true
-            }).then(() => console.log("OK"))});
-        setData([]);
-    }
 
     function deletaTudo() {
         const deletados: string[] = [];
@@ -77,8 +92,16 @@ export default function Vagas() {
     return (
         <div id="vagas">
             <Box component="section" className={styles.opcoes} sx={{p: 2}}>
-                <Button onClick={atualizaTudo} variant="contained">Atualizar tudo</Button>
                 <Button onClick={deletaTudo} variant="contained">Deletar tudo</Button>
+                <TablePagination
+                  component="div"
+                  count={data.length}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  rowsPerPage={rowsPerPage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+
             </Box>
 
             <Box hidden={!loading} sx={{width: '100%'}}>
@@ -88,33 +111,35 @@ export default function Vagas() {
             <Container hidden={loading}>
                 <ul>
                     <List sx={{width: '100%'}}>
-                        {data.map(({pred, vaga}) => {
-                            return (
-                                <ListItem
-                                    sx={{marginBottom: 2, gap: 1}}
-                                    key={vaga._id}
-                                    disablePadding
-                                >
-                                    <Chip sx={{minWidth: 70}} size="small" label={pred} variant="outlined" />
-
-                                    <ListItemButton sx={{gap: 2}}
-                                                    role={undefined}
-                                                    dense
-                                                    component="a"
-                                                    href={"https://www.linkedin.com/jobs/view/" + vaga._id}
-                                                    target={"_blank"}
+                        {data.map(({pred, vaga}, index) => {
+                            if (index < rowsPerPage) {
+                                return (
+                                    <ListItem
+                                        sx={{marginBottom: 2, gap: 1}}
+                                        key={vaga._id}
+                                        disablePadding
                                     >
-                                        <ListItemText sx={{maxWidth: 400}} id={vaga._id}
-                                                      primary={`${vaga.txtVaga}`}
-                                        />
-                                    </ListItemButton>
+                                        <Chip sx={{minWidth: 70}} size="small" label={pred} variant="outlined" />
 
-                                    <Button onClick={() => aplicar(vaga._id)}  variant="outlined" endIcon={<SendIcon />}>Aplicar</Button>
-                                    <IconButton onClick={() => deletar(vaga._id)} aria-label="delete">
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </ListItem>
-                            )
+                                        <ListItemButton sx={{gap: 2}}
+                                                        role={undefined}
+                                                        dense
+                                                        component="a"
+                                                        href={"https://www.linkedin.com/jobs/view/" + vaga._id}
+                                                        target={"_blank"}
+                                        >
+                                            <ListItemText sx={{maxWidth: 400}} id={vaga._id}
+                                                          primary={`${vaga.txtVaga}`}
+                                            />
+                                        </ListItemButton>
+
+                                        <Button onClick={() => aplicar(vaga._id)}  variant="outlined" endIcon={<SendIcon />}>Aplicar</Button>
+                                        <IconButton onClick={() => deletar(vaga._id)} aria-label="delete">
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </ListItem>
+                                )
+                            }
                         })}
                     </List>
                 </ul>
